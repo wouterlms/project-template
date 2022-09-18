1767a29a3ee5efb8750940f257ad5d2c33fec3a4
Wouter <wouter.laermans@appwise.be>
2022-09-18 09:56:23 +0200

    refactor: remove en translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,12 @@
 import { createI18n } from 'vue-i18n'
 
-import en from './locales/en.json'
 import nl from './locales/nl.json'
 
 export enum Locale {
-  EN = 'en-US',
   NL = 'nl-BE'
 }
 
 const messages = {
-  [Locale.EN]: en,
   [Locale.NL]: nl,
 }
 
