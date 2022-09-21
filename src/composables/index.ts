42f39bcca977c08806a397ec6f50a9adc35b4bd5
Wouter <wouter.laermans@appwise.be>
2022-09-21 15:39:53 +0200

    refactor

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,4 +1,4 @@
+export { default as useAuth } from './useAuth'
 export { default as useFileUploadService } from './useFileUploadService'
 export { default as useInjectOrDie } from './useInjectOrDie'
 export { default as useSuspense } from './useSuspense'
-export { default as useAuth } from './useAuth'
