469f41d259af7b4094c465158985ab7db7f9d306
Wouter <wouter.laermans@appwise.be>
2022-09-24 21:12:14 +0200

    refactor: remove direct use of localStorage and replace it by useLocalStorage

--- src/composables/useLocalStorage.ts
+++ src/composables/useLocalStorage.ts
@@ -5,8 +5,6 @@ import { useLocalStorage } from '@wouterlms/composables'
 import type { LocalStorageKey } from '@/enums'
 import type { LocalStorageValue } from '@/types'
 
-export default <T extends LocalStorageKey>(key: T): Ref<Nullable<LocalStorageValue[T]>> => {
-  const localStorageValue = useLocalStorage(key) as Ref<LocalStorageValue[T]>
-
-  return localStorageValue
-}
+export default <T extends LocalStorageKey>(key: T): Ref<Nullable<LocalStorageValue[T]>> => (
+  useLocalStorage(key) as Ref<LocalStorageValue[T]>
+)
--- src/modules/auth/login-form/composables/useLoginFormService.ts
+++ src/modules/auth/login-form/composables/useLoginFormService.ts
@@ -1,16 +1,18 @@
 import { AxiosError } from 'axios'
 import type { Form } from '@wouterlms/forms'
 
-import { useAuth } from '@/composables'
+import { useAuth, useLocalStorage } from '@/composables'
 import { useForgotPasswordStore } from '@/stores'
 
 import type { FormService, LoginFormState } from '@/types'
+import { LocalStorageKey } from '@/enums'
 
 const useLoginFormService: FormService<Form<LoginFormState>> = (formState) => {
   const { t } = useI18n()
   const { replace } = useRouter()
   const { user, signIn, getUser } = useAuth()
   const { lastLoginAttemptEmail } = useForgotPasswordStore()
+  const lastLoggedInUser = useLocalStorage(LocalStorageKey.LAST_LOGGED_IN_USER)
 
   const handleSubmit = async (): Promise<void> => {
     const { email, password } = formState.getData()
@@ -18,9 +20,10 @@ const useLoginFormService: FormService<Form<LoginFormState>> = (formState) => {
     try {
       await signIn(email, password)
       await getUser()
-      await replace('/')
 
-      localStorage.setItem('lastLoggedInUser', JSON.stringify(user.value))
+      lastLoggedInUser.value = user.value
+
+      await replace('/')
     } catch (e) {
       lastLoginAttemptEmail.value = email
 
