6a7acc41f1c209ad3b63498113768e832ae842a1
Wouter <wouter.laermans@appwise.be>
2022-09-26 19:50:33 +0200

    feat: add env type support

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,14 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_CLIENT_ID: string
+  readonly VITE_CLIENT_SECRET: string
+  readonly VITE_TEST_ACCOUNTS?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
