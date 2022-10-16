95e1c599a3421fb3a3d282057ed40cf918414caf
Wouter <wouter.laermans@appwise.be>
2022-10-16 16:21:30 +0200

    refactor: RegisterOptions.d.ts

--- src/types/web-authn/RegisterOptions.d.ts
+++ src/types/web-authn/RegisterOptions.d.ts
@@ -1,36 +1,26 @@
-interface Rp {
-  name: string
-  id: string
-}
-
-interface User {
-  id: string
-  name: string
-  displayName: string
-}
-
-interface PubKeyCredParam {
-  alg: number
-  type: 'public-key'
-}
-
-interface ExcludeCredential {
-  id: string
-  type: 'public-key'
-}
-
-interface AuthenticatorSelection {
-  requireResidentKey: boolean
-  userVerification: UserVerificationRequirement
-}
-
 export default interface RegisterOptions {
   challenge: string
-  rp: Rp
-  user: User
-  pubKeyCredParams: PubKeyCredParam[]
+  rp: {
+    name: string
+    id: string
+  }
+  user: {
+    id: string
+    name: string
+    displayName: string
+  }
+  pubKeyCredParams: {
+    alg: number
+    type: 'public-key'
+  }[]
   timeout: number
   attestation: AttestationConveyancePreference
-  excludeCredentials: ExcludeCredential[]
-  authenticatorSelection: AuthenticatorSelection
+  excludeCredentials: {
+    id: string
+    type: 'public-key'
+  }[]
+  authenticatorSelection: {
+    requireResidentKey: boolean
+    userVerification: UserVerificationRequirement
+  }
 }
