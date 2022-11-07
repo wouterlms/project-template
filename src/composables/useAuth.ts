6104bf14f4c627208fc07381d04bddccb1d65395
Wouter <wouter.laermans@appwise.be>
2022-11-07 15:17:09 +0100

    throw error when oAuth .env variables are not set

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -8,6 +8,13 @@ const {
   VITE_CLIENT_SECRET,
 } = import.meta.env
 
+if (
+  VITE_API_BASE_URL === undefined
+  || VITE_CLIENT_ID === undefined
+  || VITE_CLIENT_SECRET === undefined
+)
+  throw new Error('API Base URL, Client ID, or Client Secret is not defined. Make sure to define `VITE_API_BASE_URL`, `VITE_CLIENT_ID`, and `VITE_CLIENT_SECRET` in your .env file.')
+
 export default (): UseAuth<Profile> => useAuth<Profile>({
   baseURL: VITE_API_BASE_URL,
   clientId: VITE_CLIENT_ID,
