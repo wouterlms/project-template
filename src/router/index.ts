1342cb1a0919fdec0a3cc46d35796a1de66c2baa
Wouter <wouter.laermans@appwise.be>
2022-12-26 19:45:23 +0100

    Improvements

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,12 +31,32 @@ router.beforeEach(async (to) => {
     .map(({ meta }) => meta.middleware!)
     .flat()
 
+  const routes = router.getRoutes()
+  const routesWithStores = routes.filter(({ meta }) => meta.store != null)
+
   if (middlewares !== null) {
     for (const middleware of middlewares) {
       const result = await middleware()
 
       if (result !== undefined)
         return result
+    }
+  }
+
+  for (const routeWithStore of routesWithStores) {
+    const { path } = to
+    const { meta, path: routeWithStorePath } = routeWithStore
+
+    if (!path.startsWith(routeWithStorePath)) {
+      const { store } = meta
+      const { $id, reset } = store!()
+
+      try {
+        reset()
+      }
+      catch {
+        throw new Error(`Attempted to call \`reset()\` on ${$id}Store`)
+      }
     }
   }
 })
